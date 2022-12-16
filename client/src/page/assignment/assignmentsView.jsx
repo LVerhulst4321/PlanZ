29f4f9f79a5589086f3c1bb1c681ccad4171013d
BC Holmes <github@bcholmes.org>
2022-12-16 18:28:54 -0500

    merge with main

---
+++ client/src/page/assignment/assignmentsView.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+class AssignmentsView extends React.Component {
+
+    render() {
+        return (<div>Session id {this.props.sessionId}</div>)
+    }
+}
+
+export default AssignmentsView;
--- client/src/page/volunteer/staffVolunteerPage.jsx
+++ client/src/page/volunteer/staffVolunteerPage.jsx
@@ -4,33 +4,31 @@ import CreateVolunteerShiftModal from './createVolunteerShiftModal';
 import VolunteerJobsWidget from './volunteerJobsWidget';
 import VolunteerShiftWidget from './volunteerShiftWidget';
 
-class StaffVolunteerPage extends React.Component {
+const StaffVolunteerPage = () => {
 
-    render() {
-        return (
-            <div>
-                <div className="row">
-                    <div className="col-xl-8">
-                        <div className="card mb-3">
-                            <div className="card-body">
-                                <VolunteerShiftWidget />
+    return (
+        <div>
+            <div className="row">
+                <div className="col-xl-8">
+                    <div className="card mb-3">
+                        <div className="card-body">
+                            <VolunteerShiftWidget />
 
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-xl-4">
-                        <div className="card mb-3">
-                            <div className="card-body">
-                                <VolunteerJobsWidget />
-                            </div>
                         </div>
                     </div>
                 </div>
-                <CreateVolunteerJobModal />
-                <CreateVolunteerShiftModal />
+                <div className="col-xl-4">
+                    <div className="card mb-3">
+                        <div className="card-body">
+                            <VolunteerJobsWidget />
+                        </div>
+                    </div>
+                </div>
             </div>
-        )
-    }
+            <CreateVolunteerJobModal />
+            <CreateVolunteerShiftModal />
+        </div>
+    )
 }
 
 export default StaffVolunteerPage;
