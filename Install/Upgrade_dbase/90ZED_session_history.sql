89b532678d3823325f293d67cdb01d3aecf3bd95
BC Holmes <github@bcholmes.org>
2023-01-26 22:31:41 -0500

    create a view to encapsulate some of the thorny session history data representation; deal with the all recent edits reports -- two of the nastiest-looking reports available. Now they are pretty. Yay.

--- Install/Upgrade_dbase/90ZED_session_history.sql
+++ Install/Upgrade_dbase/90ZED_session_history.sql
@@ -30,4 +30,44 @@ select `badgeid`, `sessionid`, `moderator`, `inactivatedts`, `inactivatedbybadge
 from ParticipantOnSessionHistory
 where inactivatedbybadgeid is not null;
 
+drop table ParticipantOnSessionHistory;
+
+create view session_change_history as
+(SELECT
+    SEH.sessionid,
+    SEH.badgeid as change_by_badgeid,
+    CONCAT(CD.firstname, " ", CD.lastname) AS change_by_name,
+    CONCAT(SEC.description,
+        (CASE WHEN SEH.editdescription IS NOT NULL THEN CONCAT(" — ", SEH.editdescription)
+        ELSE ""
+        END),
+        " — status: ",
+        SS.statusname) as description,
+    SEH.timestamp as change_ts
+FROM
+         SessionEditHistory SEH
+    JOIN SessionEditCodes SEC USING (sessioneditcode)
+    JOIN SessionStatuses SS USING (statusid)
+    JOIN CongoDump CD ON CD.badgeid = SEH.badgeid
+)
+
+UNION
+
+(SELECT
+    POSH.sessionid,
+    POSH.change_by_badgeid,
+    PartCR.pubsname AS change_by_name,
+    (CASE WHEN POSH.change_type = 'insert_assignment' THEN CONCAT('Add ', PartOS.pubsname, ' to session')
+            WHEN POSH.change_type = 'remove_assignment' THEN CONCAT('Remove ', PartOS.pubsname, ' from session')
+            WHEN POSH.change_type = 'assign_moderator' THEN CONCAT('Assign ', PartOS.pubsname, ' as moderator')
+            WHEN POSH.change_type = 'remove_moderator' THEN CONCAT('Unassign ', PartOS.pubsname, ' as moderator')
+            ELSE 'Unknown action.' END) as description,
+    POSH.change_ts
+FROM
+              participant_on_session_history POSH
+         JOIN Participants PartOS ON PartOS.badgeid = POSH.badgeid
+         JOIN Participants PartCR ON PartCR.badgeid = POSH.change_by_badgeid
+);
+
+
 INSERT INTO PatchLog (patchname) VALUES ('90ZED_session_history.sql');
--- webpages/SessionHistory.php
+++ webpages/SessionHistory.php
@@ -56,45 +56,16 @@ SELECT
 EOD;
 
     $queryArray["changes"]=<<<EOD
-    (SELECT
-    SEH.badgeid as change_by_badgeid,
-    CONCAT(CD.firstname, " ", CD.lastname) AS change_by_name,
-    CONCAT(SEC.description,
-        (CASE WHEN SEH.editdescription IS NOT NULL THEN CONCAT(" — ", SEH.editdescription)
-        ELSE ""
-        END),
-        " — status: ",
-        SS.statusname) as description,
-    SEH.timestamp as change_ts,
-    DATE_FORMAT(SEH.timestamp, "%c/%e/%y %l:%i %p") AS change_ts_format
+    SELECT
+    change_by_badgeid,
+    change_by_name,
+    description,
+    change_ts,
+    DATE_FORMAT(change_ts, "%c/%e/%y %l:%i %p") AS change_ts_format
 FROM
-         SessionEditHistory SEH
-    JOIN SessionEditCodes SEC USING (sessioneditcode)
-    JOIN SessionStatuses SS USING (statusid)
-    JOIN CongoDump CD ON CD.badgeid = SEH.badgeid
+         session_change_history
 WHERE
-    SEH.sessionid=$selsessionid)
-
-UNION
-
-(SELECT
-    POSH.change_by_badgeid,
-    PartCR.pubsname AS change_by_name,
-    (CASE WHEN POSH.change_type = 'insert_assignment' THEN CONCAT('Add ', PartOS.pubsname, ' to session')
-            WHEN POSH.change_type = 'remove_assignment' THEN CONCAT('Remove ', PartOS.pubsname, ' from session')
-            WHEN POSH.change_type = 'assign_moderator' THEN CONCAT('Assign ', PartOS.pubsname, ' as moderator')
-            WHEN POSH.change_type = 'remove_moderator' THEN CONCAT('Unassign ', PartOS.pubsname, ' as moderator')
-            ELSE 'Unknown action.' END) as description,
-    POSH.change_ts,
-    DATE_FORMAT(POSH.change_ts, "%c/%e/%y %l:%i %p") AS change_ts_format
-FROM
-              participant_on_session_history POSH
-         JOIN Participants PartOS ON PartOS.badgeid = POSH.badgeid
-         JOIN Participants PartCR ON PartCR.badgeid = POSH.change_by_badgeid
-WHERE
-    POSH.sessionid=$selsessionid)
-
-
+    sessionid=$selsessionid
 ORDER BY change_ts;
 EOD;
 
