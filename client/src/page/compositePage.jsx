e52aa2534c30306bf503db0bccfd7cedf4d9d27a
anjapatel <akpatel462@gmail.com>
2023-03-02 19:16:56 -0500

    add react component for dev login

--- client/src/page/compositePage.jsx
+++ client/src/page/compositePage.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import AdminModulesPage from './admin/adminModulesPage';
-import BrainstormPage from './brainstorm/brainstormPage';
-import AssignmentsPage from './assignment/assignmentsPage';
-import StaffVolunteerPage from './volunteer/staffVolunteerPage';
-import VolunteerSignUpPage from './volunteer/volunteerSignUpPage';
-import PrintRoomScheduleConfigPage from './tool/printRoomScheduleConfig';
-import SessionEnumerationConfigPage from './tool/sessionEnumerationConfig';
+import React from "react";
+import AdminModulesPage from "./admin/adminModulesPage";
+import BrainstormPage from "./brainstorm/brainstormPage";
+import AssignmentsPage from "./assignment/assignmentsPage";
+import StaffVolunteerPage from "./volunteer/staffVolunteerPage";
+import VolunteerSignUpPage from "./volunteer/volunteerSignUpPage";
+import PrintRoomScheduleConfigPage from "./tool/printRoomScheduleConfig";
+import SessionEnumerationConfigPage from "./tool/sessionEnumerationConfig";
+import DevLoginPage from "./login/login";
 
 /**
  * Implementing this as a sort-of pauper's version of a Router. We're starting with an
@@ -17,27 +18,28 @@ import SessionEnumerationConfigPage from './tool/sessionEnumerationConfig';
  * We can revisit this pattern later.
  */
 class CompositePage extends React.Component {
-
-    render() {
-        let url = new URL(window.location.href);
-        if (url.pathname === '/StaffVolunteerPage.php') {
-            return (<StaffVolunteerPage />);
-        } else if (url.pathname === '/AdminModules.php') {
-            return (<AdminModulesPage />);
-        } else if (url.pathname === '/volunteering.php') {
-            return (<VolunteerSignUpPage />);
-        } else if (url.pathname === '/brainstorm.php') {
-            return (<BrainstormPage />);
-        } else if (url.pathname === '/assignParticipants.php') {
-            return (<AssignmentsPage />);
-        } else if (url.pathname === '/assignSessionNumberConfig.php') {
-            return (<SessionEnumerationConfigPage />);
-        } else if (url.pathname === '/printRoomScheduleConfig.php') {
-            return (<PrintRoomScheduleConfigPage />);
-        } else {
-            return (<h4>Unknown Page</h4>);
-        }
+  render() {
+    let url = new URL(window.location.href);
+    if (url.pathname === "/StaffVolunteerPage.php") {
+      return <StaffVolunteerPage />;
+    } else if (url.pathname === "/AdminModules.php") {
+      return <AdminModulesPage />;
+    } else if (url.pathname === "/volunteering.php") {
+      return <VolunteerSignUpPage />;
+    } else if (url.pathname === "/brainstorm.php") {
+      return <BrainstormPage />;
+    } else if (url.pathname === "/assignParticipants.php") {
+      return <AssignmentsPage />;
+    } else if (url.pathname === "/assignSessionNumberConfig.php") {
+      return <SessionEnumerationConfigPage />;
+    } else if (url.pathname === "/printRoomScheduleConfig.php") {
+      return <PrintRoomScheduleConfigPage />;
+    } else if (url.pathname === "/devLogin.php") {
+      return <DevLoginPage />;
+    } else {
+      return <h4>Unknown Page</h4>;
     }
+  }
 }
 
 export default CompositePage;
